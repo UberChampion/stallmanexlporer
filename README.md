9ebb63a70136ab63612ce15dcbdf965d06a3c7c2
Ryan <ryanbeltzbackup@gmail.com>
2015-01-30 17:48:21 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Stallman-Explorer
+A web browser for people like Richard Matthew Stallman, who prefer rendering the webpage in their head.
---
+++ stallman.rb
@@ -0,0 +1,15 @@
+require 'socket'
+ 
+host = 'stallman.org'
+port = 80
+path = "/index.html"
+
+request = "GET #{path} HTTP/1.0\r\n\r\n"
+
+socket = TCPSocket.open(host,port)  
+socket.print(request)               
+response = socket.read
+
+headers,body = response.split("\r\n\r\n", 2) 
+print body
+gets.chomp
