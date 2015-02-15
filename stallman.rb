dc5be75f5b46025afe317c3bc4d67f7b9e31da97
Ryan Beltz <ryanbeltzbackup@gmail.com>
2015-02-15 13:29:29 -0500

    Update stallman.rb

--- stallman.rb
+++ stallman.rb
@@ -5,13 +5,13 @@ def web
 	port = 80
 	puts "File? (EG: /index.html)"
 	path = gets.chomp
-
+	
 	request = "GET #{path} HTTP/1.0\r\n\r\n"
-
+	
 	socket = TCPSocket.open(host,port)  
 	socket.print(request)               
 	response = socket.read
-
+	
 	headers,body = response.split("\r\n\r\n", 2) 
 	print body
 	puts " "
